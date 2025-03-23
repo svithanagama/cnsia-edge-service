dbb68beddfd7fb0e01db5ea709b3bad38d82e07e
sanjayav <svithanagama@gmail.com>
2025-03-23 11:51:15 +1100

    chore: Initial commit with SpringBoot structure.

---
+++ src/main/java/me/sanjayav/polarbookshop/orderservice/cnsiaedgeservice/CnsiaEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package me.sanjayav.polarbookshop.orderservice.cnsiaedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsiaEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnsiaEdgeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sanjayav/polarbookshop/orderservice/cnsiaedgeservice/CnsiaEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sanjayav.polarbookshop.orderservice.cnsiaedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsiaEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
